c71c213ec1aec22e1cee989bab44fc8ff2d62025
André de Albuquerque <itstheandre@gmail.com>
2021-02-13 11:08:11 +0100

    Rolling background

--- starting/js/Background.js
+++ starting/js/Background.js
@@ -7,6 +7,14 @@ class Background {
   }
 
   draw() {
+    this.x -= 3;
+    // FIRST IMAGE
     image(bgImage, this.x, this.y, this.width, this.height);
+    // SECOND IMAGE - STARTS AT THE END OF THE FIRST ONE
+    image(bgImage, this.x + this.width, this.y, this.width, this.height);
+
+    if (this.x <= -this.width) {
+      this.x = 0;
+    }
   }
 }
--- starting/js/Game.js
+++ starting/js/Game.js
@@ -5,6 +5,7 @@ class Game {
   setup() {}
 
   draw() {
+    clear();
     this.background.draw();
   }
 }
